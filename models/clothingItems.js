060f120be01d571655d3e052c7f957065d625603
Alfonso <alfonsomtzpetz@gmail.com>
2026-02-16 10:56:13 -0600

    Add user and clothingItem schemas

---
+++ models/clothingItems.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  weather: { type: String, required: true, enum: ["hot", "warm", "cold"] },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("ClothingItem", clothingItemSchema);
