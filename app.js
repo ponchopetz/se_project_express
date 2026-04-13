71fe88e476c6bc670068fc9f254e07aa29de9925
Alfonso <alfonsomtzpetz@gmail.com>
2026-04-13 11:00:04 -0600

    Add rate limiting headers

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests, please try again later.",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 app.use(limiter);
 
